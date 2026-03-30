b19e9912aec81b38eb910454f2999f5a68a134ab
Pyrocarbine <aaronhan2006@gmail.com>
2026-03-29 20:31:10 -0700

    cleanup

--- src/app/api/delete-post/[id]/route.ts
+++ src/app/api/delete-post/[id]/route.ts
@@ -4,7 +4,7 @@ const sql = neon(process.env.DATABASE_URL!);
 
 export async function DELETE(req: Request, {params}: {params: Promise<{id: string}>}) {
     const { id }  = await params;
-    const request = await sql`DELETE FROM posts WHERE id = ${id}`;
+    await sql`DELETE FROM posts WHERE id = ${id}`;
     return NextResponse.json({ success: true });
 }
 

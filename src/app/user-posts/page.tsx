8587f1c67a2f22bbe02fc136a368f14ee75bb11b
Pyrocarbine <aaronhan2006@gmail.com>
2026-04-12 21:25:08 -0700

    small stylings

--- src/app/user-posts/page.tsx
+++ src/app/user-posts/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center gap-6">
+    <div className="flex flex-col items-center gap-6 mt-10">
       {posts.map(
         (post) =>
           post.user_id == session?.user?.id && (
